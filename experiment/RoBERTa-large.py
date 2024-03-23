784d1e19833acb7d4bce62085f5288d5df2c6dac
rjs0907 <rjs0907@naver.com>
2024-03-23 23:03:15 +0900

    RoBERTa-large train & inference code

--- experiment/RoBERTa-large.py
+++ experiment/RoBERTa-large.py
@@ -1,9 +1,12 @@
+# Reference from https://huggingface.co/klue/roberta-large
 # Reference from https://huggingface.co/bespin-global/klue-sroberta-base-continue-learning-by-mnr
-
+import math
+from datetime import datetime
 from datasets import load_dataset
 from sentence_transformers.readers import InputExample
 from torch.utils.data import DataLoader
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+from sentence_transformers import SentenceTransformer, models, losses
 
 klue_sts_train = load_dataset("klue", "sts", split='train[:90%]')
 klue_sts_valid = load_dataset("klue", "sts", split='train[-10%:]')
@@ -33,13 +36,41 @@ train_dataloader = DataLoader(
     shuffle=True,
     batch_size=batch_size,
 )
-
-evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
+val_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
     sts_valid,
     name="sts-valid",
 )
-
-test_evaluator =  EmbeddingSimilarityEvaluator.from_input_examples(
+test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
     sts_test,
     name="sts-test"
 )
+
+embedding = models.Transformer(
+    model_name_or_path="klue/roberta-large",
+    max_seq_length=256,
+    do_lower_case=True
+)
+pooling_model = models.Pooling(
+    embedding.get_word_embedding_dimension(),
+    pooling_mode_mean_tokens=True,
+    pooling_mode_cls_token=False,
+    pooling_mode_max_tokens=False,
+)
+model = SentenceTransformer(modules=[embedding, pooling_model])
+
+num_epochs = 4
+batch_size = 32
+output_path = "save/"+"klue-roberta-large"+"-"+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+loss= losses.CosineSimilarityLoss(model=model)
+warmup_steps = math.ceil(len(sts_train) * num_epochs / batch_size * 0.1)
+model.fit(
+    train_objectives=[(train_dataloader, loss)],
+    evaluator=val_evaluator,
+    epochs=num_epochs,
+    evaluation_steps=int(len(train_dataloader)*0.1),
+    warmup_steps=warmup_steps,
+    output_path=output_path
+)
+
+test_evaluator(model, output_path=output_path)
