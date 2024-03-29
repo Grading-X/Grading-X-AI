b80349e8d125facf11e5e30a200455460c92062a
rjs0907 <rjs0907@naver.com>
2024-03-29 23:48:08 +0900

    Load STS data & Preprocessing

--- experiment/RoBerta-large_NLI_STS.py
+++ experiment/RoBerta-large_NLI_STS.py
@@ -71,3 +71,39 @@ nli_train_dataloader = NoDuplicatesDataLoader(
     nli_train_examples,
     batch_size=batch_size,
 )
+
+klue_sts_train = load_dataset("klue", "sts", split='train[:90%]')
+klue_sts_valid = load_dataset("klue", "sts", split='train[-10%:]')
+klue_sts_test = load_dataset("klue", "sts", split="validation")
+print(len(klue_sts_train))
+print(len(klue_sts_valid))
+print(len(klue_sts_test))
+print(klue_sts_train[0])
+
+def preprocessing(dataset):
+    result = []
+    for i, data in enumerate(dataset):
+        sentence1 = data['sentence1']
+        sentence2 = data['sentence2']
+        score = (data['labels']['label'] / 5.0)  # 5점 만점, 0~5점으로 정규화
+
+        result.append(InputExample(texts=[sentence1, sentence2], label=score))
+    return result
+
+sts_train = preprocessing(klue_sts_train)
+sts_valid = preprocessing(klue_sts_valid)
+sts_test = preprocessing(klue_sts_test)
+
+train_dataloader = DataLoader(
+    sts_train,
+    shuffle=True,
+    batch_size=batch_size,
+)
+val_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
+    sts_valid,
+    name="sts-valid",
+)
+test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
+    sts_test,
+    name="sts-test"
+)
