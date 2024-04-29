19e044a189a5d768bdd618f4f117150c675b6cd5
rjs0907 <rjs0907@naver.com>
2024-04-29 22:13:54 +0900

    Word Embedding - GloVe preprocessing

---
+++ experiment/keyword/GloVe.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from konlpy.tag import Okt
+from glove import Corpus, Glove
+
+train = pd.read_table("./save/word2vec/ratings.txt")
+train = train.dropna(how='any')
+print(train.isnull().values.any())
+print("Total: ", len(train))
+
+train['document'] = train['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+okt = Okt()
+
+tokenized_data = []
+for sentence in train['document']:
+    tokenized_sentence = okt.morphs(sentence, stem=True)
+    no_stopwords_sentence = [word for word in tokenized_sentence if not word in stopwords]
+    tokenized_data.append(no_stopwords_sentence)
