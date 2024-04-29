2835ca2e2230875495e57ed3ef27617fb9b054ff
rjs0907 <rjs0907@naver.com>
2024-04-29 22:14:25 +0900

    Word Embedding - GloVe train

--- experiment/keyword/GloVe.py
+++ experiment/keyword/GloVe.py
@@ -17,3 +17,11 @@ for sentence in train['document']:
     tokenized_sentence = okt.morphs(sentence, stem=True)
     no_stopwords_sentence = [word for word in tokenized_sentence if not word in stopwords]
     tokenized_data.append(no_stopwords_sentence)
+
+corpus = Corpus()
+
+corpus.fit(tokenized_data, window=5)
+glove = Glove(no_components=100, learning_rate=0.05)
+
+glove.fit(corpus.matrix, epochs=20, no_threads=4, verbose=True)
+glove.add_dictionary(corpus.dictionary)
