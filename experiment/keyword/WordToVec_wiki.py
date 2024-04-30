97af061df1effe66f506e244551b0879c01f3b52
rjs0907 <rjs0907@naver.com>
2024-04-30 14:00:50 +0900

    Word Embedding - W2V_wiki wikipedia file check ( Colab env )

--- experiment/keyword/WordToVec_wiki.py
+++ experiment/keyword/WordToVec_wiki.py
@@ -5,3 +5,22 @@
 
 !wget https://dumps.wikimedia.org/kowiki/latest/kowiki-latest-pages-articles.xml.bz2
 !python -m wikiextractor.WikiExtractor kowiki-latest-pages-articles.xml.bz2
+
+import re
+import os
+
+def list_wiki(dirname):
+    filepaths = []
+    filenames = os.listdir(dirname)
+    for filename in filenames:
+        filepath = os.path.join(dirname, filename)
+
+        if os.path.isdir(filepath):
+            filepaths.extend(list_wiki(filepath))
+        else:
+            find = re.findall(r"wiki_[0-9][0-9]", filepath)
+            if 0 < len(find):
+                filepaths.append(filepath)
+    return sorted(filepaths)
+
+filepaths = list_wiki("text")
