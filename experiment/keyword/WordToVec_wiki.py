1ac42e7b6f414a46bb9617c9e186175d5aa591e4
rjs0907 <rjs0907@naver.com>
2024-04-30 13:56:18 +0900

    Word Embedding - W2V_wiki wikipedia dataset ( Colab env )

--- experiment/keyword/WordToVec_wiki.py
+++ experiment/keyword/WordToVec_wiki.py
@@ -2,3 +2,6 @@
 !git clone https://github.com/SOMJANG/Mecab-ko-for-Google-Colab.git
 %cd Mecab-ko-for-Google-Colab
 !bash install_mecab-ko_on_colab190912.sh
+
+!wget https://dumps.wikimedia.org/kowiki/latest/kowiki-latest-pages-articles.xml.bz2
+!python -m wikiextractor.WikiExtractor kowiki-latest-pages-articles.xml.bz2
