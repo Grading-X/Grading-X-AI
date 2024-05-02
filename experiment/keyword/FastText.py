f1ec4c5aa7cde0874050dd3d0641ec268a81da52
rjs0907 <rjs0907@naver.com>
2024-05-02 22:50:23 +0900

    Word Embedding - FastText tokenization

--- experiment/keyword/FastText.py
+++ experiment/keyword/FastText.py
@@ -11,9 +11,32 @@ Colab Environment Setting
 
 import urllib.request
 import pandas as pd
+import hgtk
 
 # 네이버 쇼핑 리뷰 데이터
 # urllib.request.urlretrieve("https://raw.githubusercontent.com/bab2min/corpus/master/sentiment/naver_shopping.txt", filename="ratings_total.txt")
 
 data = pd.read_table('ratings_total.txt', names=['ratings', 'reviews'])
 print(len(data))
+
+def token_decompose(token):
+    def special_token(consonant):
+        if consonant: return consonant
+        return '-'
+
+    decomposed_token = ''
+    for char in token:
+        try:
+            initial, neutral, final = hgtk.letter.decompose(char)
+
+            initial = special_token(initial)
+            neutral = special_token(neutral)
+            final = special_token(final)
+            decomposed_token = decomposed_token + initial + neutral + final
+        except Exception as exception:
+            if type(exception).__name__ == 'NotHangulException':
+                decomposed_token += char
+
+    return decomposed_token
+
+print(token_decompose("테스트용"))
