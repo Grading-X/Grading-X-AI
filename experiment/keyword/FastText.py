830dae993f853aafec4c61ac116d38aa3c5e3d28
rjs0907 <rjs0907@naver.com>
2024-05-02 22:42:14 +0900

    Word Embedding - FastText data download

--- experiment/keyword/FastText.py
+++ experiment/keyword/FastText.py
@@ -8,3 +8,12 @@ Colab Environment Setting
 !make
 !pip install .
 '''
+
+import urllib.request
+import pandas as pd
+
+# 네이버 쇼핑 리뷰 데이터
+# urllib.request.urlretrieve("https://raw.githubusercontent.com/bab2min/corpus/master/sentiment/naver_shopping.txt", filename="ratings_total.txt")
+
+data = pd.read_table('ratings_total.txt', names=['ratings', 'reviews'])
+print(len(data))
