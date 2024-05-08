1f1e26787e1fd602f648cd3d4a06111e8778470b
rjs0907 <rjs0907@naver.com>
2024-05-08 20:56:37 +0900

    TEST - Dummy Data

--- test.py
+++ test.py
@@ -10,6 +10,11 @@ if __name__ == '__main__':
     keyword_model = fasttext.load_model("./experiment/keyword/save/fasttext/fasttext.bin")
     okt = Okt()
 
+    # 병렬 처리 테스트용 데이터
+    answer_list = ['안녕하세요']  # 0 index에 모범 정답 할당
+    keyword = ['키워드1', '키워드2']  # 초기 키워드
+    for char in ('안', '녕', '하', '세', '요') * 6:  # 30개 답안 구성
+        answer_list.append(char)
 
 
 
