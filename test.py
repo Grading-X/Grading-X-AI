45567ae6736ab1c67bb4bb60a329791cc99a6b56
rjs0907 <rjs0907@naver.com>
2024-05-17 22:09:01 +0900

    KeyWord(Grading) - code subtraction

--- test.py
+++ test.py
@@ -4,9 +4,6 @@ from sentence_transformers import SentenceTransformer, util
 import fasttext
 from konlpy.tag import Okt
 
-def pipeline(keyword, keyword_model, okt):
-    pass
-
 if __name__ == '__main__':
     sentence_model = SentenceTransformer('experiment/similarity/save/roberta-large_NLI_STS')
     keyword_model = fasttext.load_model("./experiment/keyword/save/fasttext/fasttext.bin")
@@ -18,28 +15,7 @@ if __name__ == '__main__':
     for char in ('안', '녕', '하', '세', '요') * 6:  # 30개 답안 구성
         answer_list.append(char)
 
-    '''
-    GPU가 아닌 CPU 환경에서도 batch_size에 따라 속도 향상이 있음을 확인함 
-    답안의 길이에 따라 소요 시간이 달라짐
-    '''
-    sentence_model.encode("Warmup")
-
-    start = time.time()
-    sentence_model.encode(answer_list, batch_size=1)
-    end = time.time()
-    print("Sequential Processing Time : ", end - start)
-    print("모델이 할당된 디바이스:", sentence_model.device)
-
-    print()
-
-    start = time.time()
-    sentence_model.encode(answer_list, batch_size=len(answer_list))
-    end = time.time()
-    print("Parallel Processing Time : ", end - start)
-    print("모델이 할당된 디바이스:", sentence_model.device)
-
     embedding_list = sentence_model.encode(answer_list, batch_size=len(answer_list))
-
     student_answer = embedding_list[1:]
     print(student_answer.shape)
                                      # 1x1024   @   30x1024 transpose -> 1x30
@@ -56,10 +32,3 @@ if __name__ == '__main__':
             final_score[index] = 0
         else:
             keyword_index.append(index)
-
-    if keyword_index:
-        keyword_score = pipeline(keyword, keyword_model, okt)
-        for i, index in enumerate(keyword_index):
-            final_score[index] = keyword_score[i]
-
-
