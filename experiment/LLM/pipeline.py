b6930ba511ddd4a563c38cf34663229d0c1cd824
rjs0907 <rjs0907@naver.com>
2024-05-24 00:12:08 +0900

    LLM(Gen) - Pipeline (example_prompt)

--- experiment/LLM/pipeline.py
+++ experiment/LLM/pipeline.py
@@ -9,3 +9,8 @@ full_prompt = PromptTemplate.from_template(full_template)
 
 introduction_template = """당신은 문제 생성기입니다. 주어진 문맥정보를 바탕으로 예시의 형식을 준수하여 문제와 답안을 {N}개 생성하세요."""
 introduction_prompt = PromptTemplate.from_template(introduction_template)
+
+example_template = """다음은 문맥정보입니다:
+
+{context}"""
+example_prompt = PromptTemplate.from_template(example_template)
