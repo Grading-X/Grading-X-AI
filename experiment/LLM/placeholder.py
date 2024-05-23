8fe57a2c472dc957e94f6434847468bde7771322
rjs0907 <rjs0907@naver.com>
2024-05-24 00:09:40 +0900

    LLM(Gen) - MessagesPlaceholder + Previous Context

--- experiment/LLM/placeholder.py
+++ experiment/LLM/placeholder.py
@@ -3,10 +3,27 @@ from langchain.prompts.chat import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
+from langchain_core.messages.human import HumanMessage
+from langchain_core.messages.ai import AIMessage
 
 human_prompt = "New Question"
 human_message_template = HumanMessagePromptTemplate.from_template(human_prompt)
 
 chat_prompt = ChatPromptTemplate.from_messages(
     [MessagesPlaceholder(variable_name="conversation"), human_message_template]
+)
+
+human_message = HumanMessage(content="Previous Question")
+ai_message = AIMessage(
+    content="""\
+1. Response Example1
+
+2. Response Example2 
+
+3. Response Example3\
+"""
+)
+
+formatted_prompt = chat_prompt.format(
+    conversation=[human_message, ai_message]
 )
