61a6057150aa24cc25047946518997316be09bc2
rjs0907 <rjs0907@naver.com>
2024-05-26 12:38:56 +0900

    LLM(Gen) - extract_qa_pairs func

--- experiment/LLM/generation.py
+++ experiment/LLM/generation.py
@@ -1,7 +1,10 @@
 import os
+import re
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_openai import ChatOpenAI
 from pipeline import pipeline_prompt
+from dotenv import load_dotenv
+load_dotenv()
 
 def is_pdf(file_path):
     with open(file_path, 'rb') as file:
@@ -63,6 +66,18 @@ def problem_generation(N):
     sentence = llm.invoke(formatted_prompt).content
     return sentence
 
+def extract_qa_pairs(text):
+    qa_pattern = re.compile(r'문제:\s*(.*?)\s*답안:\s*(.*?)(?=\s*문제:|\s*$)', re.DOTALL)
+    matches = qa_pattern.findall(text)
+
+    qa_pairs = []
+
+    for match in matches:
+        question, answer = match
+        qa_pairs.append({'문제': question.strip(), '답안': answer.strip()})
+
+    return qa_pairs
+
 if __name__ == '__main__':
     file_path = "./data/example2.txt" # pdf나 txt파일 예상하고 작업진행했습니다. 어떤식으로 주셔야할듯합니다??
 
@@ -72,13 +87,21 @@ if __name__ == '__main__':
     if extension == 'PDF':
         context = processing_pdf(file_path)  # 파일에서 정보를 뽑아냅니다
         sentence = problem_generation(N=1)  # 문제를 N개 생성합니다 (너무 많이 X..., 1개 권장)
-        pass
+
+        qa_pairs = extract_qa_pairs(sentence)  # 문제, 답안을 key로 dictionary로 구성합니다
+        for pair in qa_pairs:  # 지정한 문제 수 N개만큼 dictionary로 구성되어 있습니다.
+            print(f"문제: {pair['문제']}")
+            print(f"답안: {pair['답안']}")
 
     # TXT인 경우 처리
     elif extension == 'TXT':
         context = processing_txt(file_path)  # 파일에서 정보를 뽑아냅니다
         sentence = problem_generation(N=1)  # 문제를 N개 생성합니다 (너무 많이 X..., 1개 권장)
-        pass
+
+        qa_pairs = extract_qa_pairs(sentence)  # 문제, 답안을 key로 dictionary로 구성합니다
+        for pair in qa_pairs:  # 지정한 문제 수 N개만큼 dictionary로 구성되어 있습니다.
+            print(f"문제: {pair['문제']}")
+            print(f"답안: {pair['답안']}")
 
     else:
         # 처리할 수 없는 파일 확장자인 경우입니다. -> 에러처리? 프론트/백에서 검증거치고 들어오는지?
